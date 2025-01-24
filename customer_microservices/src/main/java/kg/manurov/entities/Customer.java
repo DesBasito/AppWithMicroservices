73ff8b04ef1b67d1331ef2f957cd0fc17ef68c81
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-01-24 18:14:03 +0600

    modified some classes and pom file

--- customer_microservices/src/main/java/kg/manurov/entities/Customer.java
+++ customer_microservices/src/main/java/kg/manurov/entities/Customer.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,14 +19,10 @@ import javax.persistence.GenerationType;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "customer")
 public class Customer {
     @Id
-    @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence")
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
